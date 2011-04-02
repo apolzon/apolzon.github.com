505314e43d46b0bc87395236abbb61cc5a932564
Chris Apolzon <apolzon@gmail.com>
2011-04-01 21:55:16 -0700

    trying to deploy to hostingrails with cap

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+set :application, "blog"
+set :repository,  "p184576r"
+set :domain, "applesonthetree.com"
+set :mongrel_port, "21800"
+set :server, "applesonthetree.com"
+
+set :git_account, "apolzon"
+
+set :scm_passphrase, Proc.new { Capistrano::CLI.password_prompt('Git Password: ') }
+
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "applesonthetree.com"                          # Your HTTP server, Apache/etc
+role :app, "applesonthetree.com"
+
+default_run_options[:pty] = true
+set :repository, "git@github.com:apolzon/apolzon.github.com.git"
+set :scm, :git
+set :user, user
+
+ssh_options[:forward_agent] = true
+set :branch, "hostingrails"
+set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
+set :git_enable_submodules, 1
+set :use_sudo, false
+set :deploy_to, "/home/p184576r/apps/blog"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApolzonGithubCom::Application.config.secret_token = 'd184e2fe9cfdcb06fee8419f2b08ada772ef8533f35f5c846396c817ffa6be345ec4b6438ed9fde04294ffaff450b6eadd5e5613f7be14125562277b66ce15a9'

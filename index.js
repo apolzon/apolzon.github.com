c84845c5eac5f6907dc2e3751676644dacdf0884
Chris Apolzon <apolzon@gmail.com>
2011-05-08 20:57:44 -0700

    rendering content stored in mongolab database

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
   $("#apple").click(function() {
-    $("#welcome").toggle();
-    $("#pages").toggle();
+    $.get("https://mongolab.com/api/1/databases/apolzon_github/collections/entries?apiKey=4d11732643274c5e0cc02c38", function(data) {
+      console.log(data);
+      $("#welcome").toggle();
+      $("#pages").toggle();
+      $("#page1").html("<h1>B00!</h1>");
+      $(data).each(function(index, item) {
+        $("#page1").append("<h2>" + item["title"] + "</h2>");
+        $("#page1").append("<p>" + item["content"] + "</p>");
+      });
+    });
   });
   $("#pages").click(function() {
     $("#pages").toggle();
